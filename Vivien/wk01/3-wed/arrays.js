ffca077127c242cf73b771bbbcafb4d2a39587e0
Vivien <vivien@Viviens-MacBook-Pro.local>
2017-11-29 23:30:05 +1100

    29/11Homewrok

---
+++ Vivien/wk01/3-wed/arrays.js
@@ -0,0 +1,18 @@
+//#1
+var days_of_the_week = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
+
+//#2
+var revised = days_of_the_week.pop();
+
+var newCalendar= days_of_the_week.unshift(revised);
+
+//#3
+
+var daysOfTheWeek =[['tuesday','monday','thursday','wednesday','friday'],['saturday','sunday']];
+
+//#4
+var weekDays= daysOfTheWeek.slice(0,1);
+
+var sorted = weekDays[0].sort();
+
+
