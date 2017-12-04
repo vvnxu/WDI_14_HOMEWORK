0d59b7302c0dd0ea21a3d211990e04287bdf1269
Vivien <vivien@Viviens-MacBook-Pro.local>
2017-12-05 09:39:15 +1100

    PT planner

---
+++ Vivien/wk01/4-thu/ptPlan.js
@@ -0,0 +1,31 @@
+var trainStops =[
+	['FlindersStreet','Richmond','EastRichmond','Burnley','Hawthorn','Glenferrie'],
+	['FlagStaff','MelbourneCentral','Parliament','Richmond','Kooyong','tooronga'],
+	['SouthernCross','Richmond','SouthYarra','Prahran','Windsor']
+];
+
+var showStops=function(getOn,getOff){
+
+	var getOn=prompt('where are you getting on');
+	var getOff = prompt('where are you getting off')
+    var stops =[];
+
+
+	for (i=0; i<trainStops.length; i++){
+		if (trainStops[i].indexOf(getOn)<trainStops[i].indexOf(getOff)){
+
+			stops.push(trainStops[i].slice(trainStops[i].indexOf(getOn),trainStops[i].indexOf(getOff)+1));
+			
+			return stops 
+		}
+
+		else {
+
+			 stops.push(trainStops[i].slice(trainStops[i].indexOf(getOff),trainStops[i].indexOf(getOn)+1));
+              var goingToCity= stop.reverve
+			 return goingToCity;
+		}
+	}
+
+	
+}
