229e945d632256c31c3af28fe4a08655f2f08370
Vivien <vivien@Viviens-MacBook-Pro.local>
2017-12-19 17:01:54 +1100

    partially done

--- Vivien/wk01/4-thu/ptPlan.js
+++ Vivien/wk01/4-thu/ptPlan.js
@@ -4,7 +4,8 @@ console.log("transport");
 var lines = {
 	  alamein: ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
   glenWaverly: ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong", "Tooronga"],
-  sandringham: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]}
+  sandringham: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]
+}
 
 // user input
 var origin= prompt('where are you getting on');
@@ -18,21 +19,30 @@ var destination= prompt('where are you getting off');
 // creating journey(city to )
 debugger
 var keys =Object.keys(lines);
+// keys:alamein,glenwaverley,sandringham
+
 keys.forEach(function(key){
-	var originIndex = key.indexOf(origin);
-    var destIndex  = key.indexOf(destination);
+	//1st key:alamein 
+	var line=lines[key]
+	var originIndex = line.indexOf(origin);
+	// origin="flinders"
+	// var originIndex = 'Alamein'.indexOf('flinders')
+    var destIndex  = line.indexOf(destination);
+
  if (originIndex > -1 && destIndex>-1){
   	 var numberOfStops = destIndex - originIndex;
-  	 var journey = key.slice(originIndex, destIndex+1)
- }else if(originIndex > destIndex){
- 	numberOfStops=orginIndex -destIndex;
- 	journey=key.slice(originIndex,destIndex-1)
+  	 var journey = line.slice(originIndex, destIndex+1)
+ }else if(originIndex > -1 && destIndex>-1&&originIndex > destIndex){
+ 	numberOfStops=originIndex -destIndex;
+ 	journey=line.slice(originIndex,destIndex-1)
    }
-   outputToUser();
+   if (journey.length > 0) {
+   	outputToUser(numberOfStops,journey);
+   }
 })
 
 
-function outputToUser () {
+function outputToUser (numberOfStops,journey) {
 	
 	console.log("origin: " + origin);
 	console.log("destination: " + destination);
---
+++ Vivien/wk04/2-tue/arrays.rb
@@ -0,0 +1,22 @@
+#ARRAYS
+planeteers = ["Earth", "Wind", "Captain Planet", "Fire", "Water"]
+
+planteers[1]
+
+planteers.push("Heart")
+
+planteers -=["Captain Planet"]
+
+rangers = ["Red", "Blue", "Pink", "Yellow", "Black"]
+
+heroes=[ planeteers,rangers ]
+
+heroes.sort
+
+heroes.sample
+#HASHES
+ninja_turtle={name:"Michelangelo",
+			weapon:"Nunchuks",
+			radical:true}
+
+ninja_turtle[:age]=5000
