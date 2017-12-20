c1584f14223dda831531781cff6cae86d6c3a322
Vivien <vivien@Viviens-MacBook-Pro.local>
2017-12-20 16:59:16 +1100

    hello

--- Vivien/wk04/2-tue/arrays.rb
+++ Vivien/wk04/2-tue/arrays.rb
@@ -9,14 +9,22 @@ planteers -=["Captain Planet"]
 
 rangers = ["Red", "Blue", "Pink", "Yellow", "Black"]
 
-heroes=[ planeteers,rangers ]
-
+heroes= planeteers+rangers
 heroes.sort
-
+heroes.shuffle
 heroes.sample
+#heroes.select{|elem| elem[0]=="B"} 
+
+
 #HASHES
 ninja_turtle={name:"Michelangelo",
-			weapon:"Nunchuks",
-			radical:true}
+			  weapon:"Nunchuks",
+			  radical:true}
 
 ninja_turtle[:age]=5000
+ninja_turtle.delete(:radical)
+ninja_turtle[pizza_topping]=["cheese", "pepperoni", "peppers"]
+ninja_turtle[pizza_topping].first
+ninja_turtle.keys
+ninja_turtle.each do |key,value|
+	puts "#{key} is equal to #{value}"
