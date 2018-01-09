2f2c31b9545ccd3586e45029bc9633128b0b949c
Vivien <vivien@Viviens-MacBook-Pro.local>
2018-01-10 09:12:20 +1100

    finished but not working

---
+++ Vivien/wk05/2-tue/movie_app/main.rb
@@ -0,0 +1,41 @@
+
+require 'sinatra'
+require "sinatra/reloader"
+require "httparty"
+
+get '/' do
+  erb :index
+end
+
+get '/search' do
+  response = HTTParty.get('http://omdbapi.com/?apikey=f1d22a2f&s=#{params[:title]}').parsed_response
+  #an hash'search' with value of an array with multiple elements in 
+
+  @info=response['search']
+
+  erb :search
+  #return @info.inspect
+
+
+end
+get '/result' do
+  result = HTTParty.get('http://omdbapi.com/?apikey=f1d22a2f&t=#{params[:title]}')
+  @title=result['Title']
+  @released=result['Released']
+  @rating=result['Runtime']
+  @poster=result['Poster']
+  @description=result['Plot']
+
+  file=File.open('history.txt','a')
+  file.puts(@title)
+  file.close
+  erb :result
+end
+
+get '/history' do
+  file=File.readlines('history.txt')
+  @list=file
+  erb:history
+
+
+end
