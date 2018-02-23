2beee7117b3410b5a1e04dd539a1f2ae946f4ce7
Vivien <vivien@Viviens-MacBook-Pro.local>
2018-02-23 13:50:14 +1100

    Homework All Done

--- Vivien/wk10/5-fri/app.js
+++ Vivien/wk10/5-fri/app.js
@@ -1,1 +1,15 @@
 
+var request = require('request');
+var city = process.argv[2];
+var apiKey= process.env.OPEN_WEATHER;
+var url='http://api.openweathermap.org/data/2.5/weather?q=' + city + ',Australia&units=metric&appid=' + apiKey
+request(url,function(err,res,body){
+	if (err ) {
+		console.log(error);
+		
+	} else {
+		var data = JSON.parse(body).main.temp
+	
+		console.log('Temperature in ' + city + ' is :', data);
+	}
+  
--- Vivien/wk11/3-wed/component/app.js
+++ Vivien/wk11/3-wed/component/app.js
@@ -1,3 +1,16 @@
 import React from 'react'
 import Password from './Password'
 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+   
+  }
+
+  render() {
+    return <div>
+      
+      <Password />
+ 
+    </div>
+   }
